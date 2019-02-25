efa6717b6325c19d940b65f0276d40e99b3eec56
dafer154 <david_fza@hotmail.com>
2019-02-25 00:29:23 -0500

    Updates

---
+++ precache-manifest.c748c6a1511c8a3605941ce4071b1821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be04a125aee1869f9dcc",
+    "url": "/app-rick-morty/static/js/runtime~main.be04a125.js"
+  },
+  {
+    "revision": "cf4d849e63dbed09aba3",
+    "url": "/app-rick-morty/static/js/main.cf4d849e.chunk.js"
+  },
+  {
+    "revision": "832efb7f417c9976277f",
+    "url": "/app-rick-morty/static/js/2.832efb7f.chunk.js"
+  },
+  {
+    "revision": "cf4d849e63dbed09aba3",
+    "url": "/app-rick-morty/static/css/main.56203c08.chunk.css"
+  },
+  {
+    "revision": "832efb7f417c9976277f",
+    "url": "/app-rick-morty/static/css/2.38bb2c49.chunk.css"
+  },
+  {
+    "revision": "960103d4233147ed527f71abe6999506",
+    "url": "/app-rick-morty/index.html"
+  }
+];
